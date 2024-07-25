cfefc7074c66b1a58f90f9a8230dab10b6c29c95
Lucas Martins <91044017+lucastuxnet@users.noreply.github.com>
2024-07-25 08:46:43 -0300

    Create vpn_client.sh

---
+++ vpn_client.sh
@@ -0,0 +1,36 @@
+
+#!/bin/bash
+
+# Atualizar os repositórios e instalar o OpenVPN.
+sudo apt-get update && \
+sudo apt-get install -y openvpn && \
+
+# Criar diretório para as chaves.
+sudo mkdir -p /etc/openvpn/keys && \
+cd /etc/openvpn/keys
+
+# Solicitar o IP remoto ao usuário
+read -p "Digite o endereço IP_1 do servidor: " IP_1
+read -p "Digite o endereço IP_2 do servidor: " IP_2
+read -p "Digite o endereço IP remoto: " IP_REMOTO
+
+# Criar arquivo de configuração do servidor
+cd /etc/openvpn/client
+sudo tee client.conf > /dev/null <<EOL
+dev tun
+proto udp
+ifconfig $IP_1 $IP_2
+remote $IP_REMOTO
+secret /etc/openvpn/keys/openvpn.key
+route 192.168.1.0 255.255.255.0
+keepalive 10 120
+daemon
+EOL
+
+# Adicionando na iptables
+sudo iptables -A INPUT -p udp --dport 1194 -j ACCEPT
+sudo iptables -A INPUT -i tun+ -j ACCEPT
+sudo iptables -A FORWARD -i tun+ -j ACCEPT
+
+# Rodar o OpenVPN com a configuração criada
+sudo systemctl start openvpn-server@server
